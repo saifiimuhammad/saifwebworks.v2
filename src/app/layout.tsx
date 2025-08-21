16da513501e7fcb2f96aeeec9754fba233ece333
saifiimuhammad <muhammadsaifarain786@gmail.com>
2025-08-21 19:06:13 +0500

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const SITE_URL = "https://0xsaif.vercel.app";
+const SITE_URL = "https://saifwebworks.vercel.app";
 
 export const metadata: Metadata = {
   title:
