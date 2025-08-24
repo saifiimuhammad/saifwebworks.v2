8c94d331d75007181778a058b937ea5ab56f9593
saifiimuhammad <muhammadsaifarain786@gmail.com>
2025-08-25 01:39:38 +0500

    Enable Analytics & Speed Insights. Improved the SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -92,6 +94,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
